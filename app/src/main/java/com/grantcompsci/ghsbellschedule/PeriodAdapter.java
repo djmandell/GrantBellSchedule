86bdf5b0e1ed50475a8b21d1d20cf0b5d85fa4bf
djmandell <dmandell@pps.net>
2022-01-19 21:28:07 -0800

    1.5.4: Properly calculate passing periods

--- app/src/main/java/com/grantcompsci/ghsbellschedule/PeriodAdapter.java
+++ app/src/main/java/com/grantcompsci/ghsbellschedule/PeriodAdapter.java
@@ -34,7 +34,6 @@ public class PeriodAdapter extends RecyclerView.Adapter<PeriodAdapter.PeriodView
         mTodayDateNumber = todayDateNumber;
     }
 
-
     public PeriodAdapter(Period[] periods){
         mPeriods=periods;
     }
@@ -56,9 +55,29 @@ public class PeriodAdapter extends RecyclerView.Adapter<PeriodAdapter.PeriodView
 
     @Override
     public void onBindViewHolder(PeriodViewHolder holder, int position) {
-        holder.bindPeriod(mPeriods[position]);
+        Date periodStartTimeDate = new Date();
+        Date lastPeriodEndTimeDate = new Date();
+
+        // default passing period length, only used for period 1 or if something is broken somehow
+        int passingPeriodMinutes = 10;
+
+
+        // if we're in any period after period 1 determine the length of the passing period (in minutes)
+        if (position !=0){
+            try {
+                // get our current period's start time and the last period's end time.  Find the difference between them in milliseconds, convert to minutes
+                periodStartTimeDate = new SimpleDateFormat("hh:mma").parse(mPeriods[position].getPeriodStart());
+                lastPeriodEndTimeDate = new SimpleDateFormat("hh:mma").parse(mPeriods[position-1].getPeriodEnd());
+                long diffinMillis = periodStartTimeDate.getTime() - lastPeriodEndTimeDate.getTime() ;
+                passingPeriodMinutes = (int)(diffinMillis/1000/60);
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+            holder.bindPeriod(mPeriods[position], passingPeriodMinutes);
     }
-    //
+
     @Override
     public int getItemCount() {
         return mPeriods.length;
@@ -80,16 +99,17 @@ public class PeriodAdapter extends RecyclerView.Adapter<PeriodAdapter.PeriodView
 
         }
 
-        public void bindPeriod(Period period){
+        // sets the start and end time for each period (assuming we have a start and end time, determines current period (if applicable) and highlights it
+        public void bindPeriod(Period period, int passingPeriodMinutes){
             //SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mma");
             Calendar selectedDateCalendar = Calendar.getInstance();
             Calendar calendarPeriodStart = Calendar.getInstance();
-            Calendar calendarPeriodEnd = Calendar.getInstance();
+            //Calendar calendarPeriodEnd = Calendar.getInstance();
             Date periodStartTimeDate = new Date();
             Date periodEndTimeDate = new Date();
             Date selectedDate = new Date();
             String selectedDateString = new SimpleDateFormat("hh:mma").format(selectedDateCalendar.getTime());
-            //!! Log.i(TAG,"Selected date =====================>" + selectedDateString);
+
             /* Highlight the current period in the period schedule display
                We check the current time (selectedDateString) and make sure it lays somewhere between current periodStart and periodEnd.
 
@@ -97,22 +117,18 @@ public class PeriodAdapter extends RecyclerView.Adapter<PeriodAdapter.PeriodView
                periodEnd and the selectedDateString and converts them to Date objects so that they can be compared.
 
              */
+
+            // Sometimes we don't have a period start or end time (PSAT day has weird schedules, late start has "check PPS" message)
+            // Only try to parse period start/end if there is a period start/end
             if (!period.getPeriodStart().equals(" ") && !period.getPeriodStart().equals("")) {
-
-
                 try {
-                    // Sometimes we don't have a period start or end time (PSAT day has weird schedules, late start has "check PPS" message)
-                    // Only try to parse period start/end if there is a period start/end
-
-
+                    // parse Date, convert Date to Calendar so we can easily subtract a few minutes from the start time, then convert back to Date again.
+                    // There was probably an easier way to do this, right?
                     periodStartTimeDate = new SimpleDateFormat("hh:mma").parse(period.getPeriodStart());
-
-                    if (!period.getPeriodName().equals("LUNCH")) {
-                        calendarPeriodStart.setTime(periodStartTimeDate);
-                        calendarPeriodStart.add(Calendar.MINUTE, -5);
-                        periodStartTimeDate = calendarPeriodStart.getTime();
-                    }
-
+                    calendarPeriodStart.setTime(periodStartTimeDate);
+                    calendarPeriodStart.add(Calendar.MINUTE, - passingPeriodMinutes);
+                    periodStartTimeDate = calendarPeriodStart.getTime();
+                    // Parse period end time and today's date
                     periodEndTimeDate = new SimpleDateFormat("hh:mma").parse(period.getPeriodEnd());
                     selectedDate = new SimpleDateFormat("hh:mma").parse(selectedDateString);
 
@@ -128,7 +144,7 @@ public class PeriodAdapter extends RecyclerView.Adapter<PeriodAdapter.PeriodView
                 mNameLabel.setGravity(Gravity.LEFT);
             }
 
-            /*Check to see if the current time is before the end of the period and after the start (-5 minutes).
+            /*Check to see if the current time is before the end of the period and after the start (minus the passing period time).
               If so, the relativeLayout's background color will be blue and all the text will be white.
              */
             if((mTodayDateNumber == mDateNumber && selectedDate.before(periodEndTimeDate) ) && ((selectedDate.after(periodStartTimeDate) || selectedDate.equals(periodStartTimeDate)) ) ){
@@ -140,13 +156,13 @@ public class PeriodAdapter extends RecyclerView.Adapter<PeriodAdapter.PeriodView
 
             }
 
-
+            // set the labels in the interface for the period name, the period start time, and the period end time
             mNameLabel.setText(period.getPeriodName());
             mStartLabel.setText(period.getPeriodStart());
             mEndLabel.setText(period.getPeriodEnd());
 
-//
-
         }
+
+
     }
 }
